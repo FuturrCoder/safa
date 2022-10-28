d39dc65cb2610a4bb1cf72cdf1e9cadf753ab160
FuturrCoder <33908899+FuturrCoder@users.noreply.github.com>
2022-10-28 12:33:08 -0400

    Initial Commit

---
+++ SAFA/SAFAApp.swift
@@ -0,0 +1,17 @@
+//
+//  SAFAApp.swift
+//  SAFA
+//
+//  Created by Mirai DTN on 10/28/22.
+//
+
+import SwiftUI
+
+@main
+struct SAFAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
