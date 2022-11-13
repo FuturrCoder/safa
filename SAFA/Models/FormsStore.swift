97d633c5d46287fd27ababe42978faaef66bb086
FuturrCoder <33908899+FuturrCoder@users.noreply.github.com>
2022-11-13 18:48:19 -0500

    Add FormsStore

---
+++ SAFA/Models/FormsStore.swift
@@ -0,0 +1,12 @@
+//
+//  FormsStore.swift
+//  SAFA
+//
+//  Created by Mirai DTN on 11/13/22.
+//
+
+import SwiftUI
+
+class FormsStore: ObservableObject {
+    @Published var forms: [ApplicationForm] = []
+}
