0e993b99083f90c1c2b524c6b4f093c8f1b3c62a
FuturrCoder <33908899+FuturrCoder@users.noreply.github.com>
2022-11-13 12:09:19 -0500

    Update ApplicationForm and add sample data

--- SAFA/Models/ApplicationForm.swift
+++ SAFA/Models/ApplicationForm.swift
@@ -9,11 +9,11 @@ import Foundation
 
 struct ApplicationForm: Identifiable, Codable {
     let id: UUID
-    var questions: [FormItem]
+    var items: [FormItem]
     
-    init(id: UUID = UUID(), questions: [FormItem]) {
+    init(id: UUID = UUID(), items: [FormItem]) {
         self.id = id
-        self.questions = questions
+        self.items = items
     }
 }
 
@@ -21,15 +21,59 @@ struct FormItem: Identifiable, Codable {
     let id: UUID
     let prompt: String
     var response: Response
+    let optional: Bool
+    /// Whether the question has been answered
+    var answered: Bool
+    
+    init(id: UUID = UUID(), prompt: String, response: Response, optional: Bool = false, answered: Bool = true) {
+        self.id = id
+        self.prompt = prompt
+        self.response = response
+        self.optional = optional
+        self.answered = answered
+    }
 }
 
 extension FormItem {
     enum Response: Codable {
-        case number(Int)
-        case date(Date)
+        case number(Int, ClosedRange<Int>)
+        case date(Date, DateInterval)
+        /// Currently selected, list of options
+        case menu(Int, [String])
         case shortAnswer(String)
         case longAnswer(String)
-        case image(URL)
-        case video(URL)
+        case image(URL?)
+        case video(URL?)
     }
+}
+
+extension ApplicationForm {
+    private static var f: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy"
+        return formatter
+    }
+    static let samplePersonal: [FormItem] = [
+        FormItem(prompt: "What is your legal name?", response: .shortAnswer("")),
+        FormItem(prompt: "What is your preferred nickname?", response: .shortAnswer(""), optional: true),
+        FormItem(prompt: "What is your gender?", response: .menu(0, ["Male", "Female", "Other/Non-binary"])),
+        FormItem(prompt: "What is your date of birth?",
+                 response: .date(f.date(from: "1970")!, DateInterval(start: f.date(from: "1900")!, end: Date()))),
+        FormItem(prompt: "Upload a profile photo", response: .image(nil))
+    ]
+    static let sampleVideos: [FormItem] = [
+        FormItem(prompt: "Upload a video of your dribbling", response: .video(nil)),
+        FormItem(prompt: "Upload a video of your penalty kick", response: .video(nil))
+    ]
+    static let samplePreference: [FormItem] = [
+        FormItem(prompt: "About how many years have you been playing football for?", response: .number(0, 0...100)),
+        FormItem(prompt: "Why do you want to go to a football academy?", response: .longAnswer("")),
+        FormItem(prompt: "What do you look for in a football academy?", response: .longAnswer("")),
+        FormItem(prompt: "What is one of your greatest accomplishments?", response: .longAnswer(""))
+    ]
+    static let sampleData: [ApplicationForm] = [
+        ApplicationForm(items: samplePersonal),
+        ApplicationForm(items: sampleVideos),
+        ApplicationForm(items: samplePreference)
+    ]
 }
